8370692604b10349fc83dde83863bc9bbf4b93c8
erenHeech <59767110+erenkaraymir@users.noreply.github.com>
2023-06-17 14:51:26 +0300

    Add : Gold Manager

---
+++ Assets/Scripts/Ymir Dream Base Project/GoldManager/GoldManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldManager : SingletonDestroyMono<GoldManager>
+{
+    public event Action<int> OnGoldChanged; // Altýn miktarý deðiþtiðinde tetiklenecek olay
+
+    private int _currentGold; // Geçerli altýn miktarý
+
+    [SerializeField] private int _beginningGold;
+
+    private void Start()
+    {
+        _currentGold = _beginningGold;
+    }
+
+    public int GetGoldAmount()
+    {
+        return _currentGold;
+    }
+
+    public void AddGold(int amount)
+    {
+        _currentGold += amount;
+
+        // Altýn miktarý deðiþtiðinde olayý tetikler
+        OnGoldChanged?.Invoke(_currentGold);
+    }
+
+    public void SpendGold(int amount)
+    {
+        if (CanAfford(amount))
+        {
+            _currentGold -= amount;
+
+            // Altýn miktarý deðiþtiðinde olayý tetikler
+            OnGoldChanged?.Invoke(_currentGold);
+        }
+
+    }
+
+    public bool CanAfford(int amount)
+    {
+        return _currentGold >= amount;
+    }
+
+    public void ResetGold()
+    {
+        _currentGold = 0;
+
+        // Altýn miktarý deðiþtiðinde olayý tetikler
+        OnGoldChanged?.Invoke(_currentGold);
+    }
+}
